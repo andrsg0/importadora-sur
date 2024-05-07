ad57c62066b4479a96a4e8c25bd266151f10417a
andrsLaptopWindows <andres_g0@outlook.com>
2024-05-07 17:45:13 -0500

    Mostrar la vista del producto al usuario

--- src/main/java/com/abcg/controller/HomeController.java
+++ src/main/java/com/abcg/controller/HomeController.java
@@ -1,24 +1,32 @@
 package com.abcg.controller;
 
 import com.abcg.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private ProductService productService;
 
     @GetMapping("")
     public String home(Model model){
-
         model.addAttribute("products", productService.findAll());
-
         return "user/home";
+    }
+
+    @GetMapping("producthome/{id}")
+    public String productHome(@PathVariable Integer id){
+        log.info("Id producto enviado como parametro");
+        return "user/producthome";
     }
 }
