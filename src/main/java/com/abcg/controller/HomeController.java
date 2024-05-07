34d19237e946251820419588f735cf3ab62122b2
andrsLaptopWindows <andres_g0@outlook.com>
2024-05-07 18:48:22 -0500

    Mostrar informacion del producto en productohome@

--- src/main/java/com/abcg/controller/HomeController.java
+++ src/main/java/com/abcg/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.abcg.controller;
 
+import com.abcg.model.Product;
 import com.abcg.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -25,8 +28,13 @@ public class HomeController {
     }
 
     @GetMapping("producthome/{id}")
-    public String productHome(@PathVariable Integer id){
+    public String productHome(@PathVariable Integer id, Model model){
         log.info("Id producto enviado como parametro");
+        Product product = new Product();
+        Optional<Product> productOptional = productService.get(id);
+        product = productOptional.get();
+        model.addAttribute("product", product);
+
         return "user/producthome";
     }
 }
