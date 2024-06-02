9b1e9231334c2df2ac50f1642cb71b0bdf361374
andrsDesktopWindows <andres_g0@outlook.com>
2024-06-01 20:04:37 -0500

    Funcionalidad para ver ordenes y detalles para el admin

--- src/main/java/com/abcg/controller/AdminController.java
+++ src/main/java/com/abcg/controller/AdminController.java
@@ -1,12 +1,17 @@
 package com.abcg.controller;
 
+import com.abcg.model.Order;
 import com.abcg.model.Product;
+import com.abcg.service.IOrderService;
 import com.abcg.service.IProductService;
 import com.abcg.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,6 +25,12 @@ public class AdminController {
     @Autowired
     private IUserService userService;
 
+    @Autowired
+    private IOrderService orderService;
+
+    private Logger log = LoggerFactory.getLogger(AdminController.class);
+
+
     @GetMapping("")
     public String home(Model model){
         List<Product> products = IProductService.findAll();
@@ -31,5 +42,19 @@ public class AdminController {
     public String users(Model model){
         model.addAttribute("users", userService.findAll());
         return "admin/users";
+    }
+
+    @GetMapping("/orders")
+    public String orders(Model model){
+        model.addAttribute("orders", orderService.findAll());
+        return "admin/orders";
+    }
+
+    @GetMapping("details/{id}")
+    public String detail(Model model, @PathVariable Integer id){
+        log.info("Id de la orden: {}", id);
+        Order order = orderService.findById(id).get();
+        model.addAttribute("details", order.getDetail());
+        return "admin/orderdetail";
     }
 }
